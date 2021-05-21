abedd9137aa1bd251a57d619e5d62030852dda7d
Jamie Ellis <78352327+Jamie-Ellis@users.noreply.github.com>
2021-05-21 15:33:38 +0200

    adds library page and actions

--- src/actions/index.js
+++ src/actions/index.js
@@ -7,21 +7,65 @@
 // and the function that you will return out of an action creator
 // will receive from redux TWO PARAMETERS: dispatch and getState
 
-export const addToLikedSongs = (track) => ({
-    type: 'ADD_SONG_TO_LIKED', // mandatory property
-    payload: track, // not mandatory property, for additional information
-  })
-  
-  export const addToLikedSongsActionWithThunk = (track) => {
-    return (dispatch, getState) => {
-      // put here your logic!!
-      console.log("I'm dispatching this out of a function!", getState())
-      // fetches, api calls, etc.
-      if (true) {
-        dispatch({
-          type: 'ADD_SONG_TO_LIKED', // mandatory property
-          payload: track, // not mandatory property, for additional information
-        })
+export const addToLikedSongs = (song) => ({
+  type: "ADD_SONG_TO_LIKED", // mandatory property
+  payload: song, // not mandatory property, for additional information
+});
+
+export const addToLikedSongsActionWithThunk = (song) => {
+  return (dispatch, getState) => {
+    // put here your logic!!
+    console.log("I'm dispatching this out of a function!", getState());
+    // fetches, api calls, etc.
+    if (song) {
+      dispatch({
+        type: "ADD_SONG_TO_LIKED", // mandatory property
+        payload: song, // not mandatory property, for additional information
+      });
+    }
+  };
+};
+
+export const removeFromLikedSongsAction = (index) => ({
+  type: "REMOVE_SONG_FROM_LIKED",
+  payload: index,
+});
+
+export const getSongsAction = (value) => {
+  return async (dispatch, getState) => {
+    if (value) {
+      console.log("fetching the songs...");
+      try {
+        let resp = await fetch(
+          "https://striveschool-api.herokuapp.com/food-bosoks"
+        );
+        if (resp.ok) {
+          let songs = await resp.json();
+          dispatch({
+            type: "GET_SONGS",
+            payload: songs,
+          });
+        } else {
+          console.log("error");
+          setTimeout(() => {
+            dispatch({
+              type: "GET_SONGS_TOGGLE_ERROR",
+            });
+            setTimeout(() => {
+              dispatch({
+                type: "GET_SONGS_TOGGLE_ERROR",
+              });
+            }, 5000);
+          }, 1000);
+        }
+      } catch (error) {
+        console.log(error);
+        setTimeout(() => {
+          dispatch({
+            type: "GET_SONGS_TOGGLE_ERROR",
+          });
+        }, 1000);
       }
     }
-  }
+  };
+};
