5a3a06315c72f79eaad9abc31684fd73df1d23a1
bioback <78535998+bioback@users.noreply.github.com>
2021-05-21 14:59:39 +0200

    song info

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,45 +1,62 @@
 import React from "react";
-import { Row } from "react-bootstrap";
+import { Col, Row } from "react-bootstrap";
 
 const Player = () => (
   <div className="container-fluid fixed-bottom bg-container pt-1">
     <Row>
-      <div className="col-lg-10 offset-lg-2">
-        <Row>
-          <div className="col-6 col-md-4 col-lg-2 offset-3 offset-md-4 offset-lg-5 playerControls mt-1">
-            <Row>
-              <a href="/">
-                <img src="/playerbuttons/Shuffle.png" alt="shuffle" />
-              </a>
-              <a href="/">
-                <img src="/playerbuttons/Previous.png" alt="shuffle" />
-              </a>
-              <a href="/">
-                <img src="/playerbuttons/Play.png" alt="shuffle" />
-              </a>
-              <a href="/">
-                <img src="/playerbuttons/Next.png" alt="shuffle" />
-              </a>
-              <a href="/">
-                <img src="/playerbuttons/Repeat.png" alt="shuffle" />
-              </a>
-            </Row>
+      <div className="col-lg-10 offset-lg-2 d-flex">
+        <div className="col-2 d-flex">
+          <img
+            className="align-self-center"
+            src="https://image-placeholder.com/images/actual-size/57x57.png"
+            style={{ height: "58px" }}
+          ></img>
+          <div className="d-flex flex-column">
+            <span className="text-nowrap text-white ml-4">SONG TITLE</span>
+            <span className="text-nowrap text-white ml-4">SONG ARTIST</span>
+            <span className="text-nowrap text-white ml-4">SONG ALBUM</span>
           </div>
-        </Row>
-        <Row className="justify-content-center playBar py-3">
-          <div className="col-8 col-md-6">
-            <div id="progress">
-              <div
-                className="progress-bar"
-                role="progressbar"
-                aria-valuenow={0}
-                aria-valuemin={0}
-                aria-valuemax={100}
-              ></div>
+        </div>
+        <div className="col">
+          <Row>
+            <div className="col-6 col-md-4 col-lg-2 offset-3 offset-md-4 offset-lg-5 playerControls mt-1">
+              <Row>
+                <a href="/">
+                  <img src="/playerbuttons/Shuffle.png" alt="shuffle" />
+                </a>
+                <a href="/">
+                  <img src="/playerbuttons/Previous.png" alt="shuffle" />
+                </a>
+                <a href="/">
+                  <img src="/playerbuttons/Play.png" alt="shuffle" />
+                </a>
+                <a href="/">
+                  <img src="/playerbuttons/Next.png" alt="shuffle" />
+                </a>
+                <a href="/">
+                  <img src="/playerbuttons/Repeat.png" alt="shuffle" />
+                </a>
+              </Row>
             </div>
-          </div>
-        </Row>
+          </Row>
+          <Row className="justify-content-center playBar py-3">
+            <div className="col-8 col-md-6">
+              <div id="progress">
+                <div
+                  className="progress-bar"
+                  role="progressbar"
+                  aria-valuenow={0}
+                  aria-valuemin={0}
+                  aria-valuemax={100}
+                ></div>
+              </div>
+            </div>
+          </Row>
+        </div>
       </div>
+      {/* <div className="col-lg-2 offset-lg-2">
+        <img src="https://image-placeholder.com/images/actual-size/57x57.png"></img>
+      </div> */}
     </Row>
   </div>
 );
