15555981ded3dd6ad1772110dcc2f532b9601c2e
Jamie Ellis <78352327+Jamie-Ellis@users.noreply.github.com>
2021-05-21 16:08:18 +0200

    adds library page

--- src/components/YourLibrary.jsx
+++ src/components/YourLibrary.jsx
@@ -1,84 +1,55 @@
 import React from "react";
 import Song from "./Song";
-import { Row } from "react-bootstrap";
-import { connect } from 'react-redux'
-import { removeFromCartAction } from '../actions'
+import { FaTrash } from "react-icons/fa";
+import { Row, Button } from "react-bootstrap";
+import { connect } from "react-redux";
+import { removeFromLikedSongsAction } from "../actions";
 
+const mapStateToProps = (state) => state;
 
-class Album extends React.Component {
-  state = {
-    album: {},
-    songs: [],
-  };
-
-  componentDidMount = async () => {
-    let albumId = this.props.match.params.id;
-
-    let headers = new Headers({
-      "X-RapidAPI-Host": "deezerdevs-deezer.p.rapidapi.com",
-      "X-RapidAPI-Key": "222902beabmshb95a65b737cead6p1f3ac9jsn23ced94c0d20",
-    });
-
-    try {
-      let response = await fetch(
-        "https://striveschool-api.herokuapp.com/api/deezer/album/" + albumId,
-        {
-          method: "GET",
-          headers,
-        }
-      );
-
-      if (response.ok) {
-        let album = await response.json();
-        this.setState({
-          album,
-          songs: album.tracks.data,
-        });
-      }
-    } catch (exception) {
-      console.log(exception);
-    }
-  };
-
-  render() {
-    return (
-      <div className="col-12 col-md-9 offset-md-3 mainPage">
-        <Row className="mb-3">
-          <div className="col-9 col-lg-11 mainLinks d-none d-md-flex">
-            <div>TRENDING</div>
-            <div>PODCAST</div>
-            <div>MOODS AND GENRES</div>
-            <div>NEW RELEASES</div>
-            <div>DISCOVER</div>
+const mapDispatchToProps = (dispatch) => ({
+  removeFromLikedSongs: (index) => dispatch(removeFromLikedSongsAction(index)),
+});
+const LibraryPage = ({ library, removeFromLikedSongsAction }) => (
+  <>
+    <div className="col-12 col-md-9 offset-md-3 mainPage">
+      <Row className="mb-3">
+        <div className="col-9 col-lg-11 mainLinks d-none d-md-flex">
+          <div>TRENDING</div>
+          <div>PODCAST</div>
+          <div>MOODS AND GENRES</div>
+          <div>NEW RELEASES</div>
+          <div>DISCOVER</div>
+        </div>
+      </Row>
+      <Row>
+        <div className="col-md-3 pt-5 text-center" id="img-container">
+          <div className="mt-4 text-center">
+            <p className="album-title">Your Library</p>
           </div>
-        </Row>
-        <Row>
-          {this.state.album.cover && (
-            <div className="col-md-3 pt-5 text-center" id="img-container">
-              <div className="mt-4 text-center">
-                <p className="album-title">Your Library</p>
-              </div>
-              <div className="mt-4 text-center">
-                <button id="btnPlay" className="btn btn-success" type="button">
-                  Play
-                </button>
-              </div>
+          <div className="mt-4 text-center">
+            <button id="btnPlay" className="btn btn-success" type="button">
+              Play
+            </button>
+          </div>
+        </div>
+
+        <div className="col-md-8 p-5">
+          <Row>
+            <div className="col-md-10 mb-5" id="trackList">
+              {library.favourites.map((song, i) => (
+                <Song track={song} key={i}>
+                  <Button variant="danger" onClick={() => removeFromLikedSongsAction(i)}>
+                    <FaTrash />
+                  </Button>
+                </Song>
+              ))}
             </div>
-          )}
-          <div className="col-md-8 p-5">
-            <Row>
-              <div className="col-md-10 mb-5" id="trackList">
-                {this.state.songs.map((song) => (
-                  <Song track={song} key={song.id}>
-                  </Song>
-                ))}
-              </div>
-            </Row>
-          </div>
-        </Row>
-      </div>
-    );
-  }
-}
+          </Row>
+        </div>
+      </Row>
+    </div>
+  </>
+);
 
-export default Album;
+export default connect(mapStateToProps, mapDispatchToProps)(LibraryPage);
