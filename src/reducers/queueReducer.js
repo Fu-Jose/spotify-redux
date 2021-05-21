5f7f4045da8b95e965436de65ed5f4289ac4280f
Jamie Ellis <78352327+Jamie-Ellis@users.noreply.github.com>
2021-05-21 14:12:27 +0200

    adds cases to reducers

--- src/reducers/queueReducer.js
+++ src/reducers/queueReducer.js
@@ -2,8 +2,18 @@ import { initialState } from "../store";
 
 const queueReducer = (state = initialState.songQueue, action) => {
   switch (action.type) {
+    case 'ADD_SONG_TO_QUEUE':
+      return {
+        ...state,
+        queue: [...state.queue, action.payload],
+      }
+    case 'REMOVE_SONG_FROM_QUEUE':
+      return {
+        ...state,
+        queue: [...state.queue.slice(0, action.payload), ...state.queue.slice(action.payload + 1)],
+      }
     default:
-      return state;
+      return state
   }
 };
 
--- src/reducers/songPlayingReducer.js
+++ src/reducers/songPlayingReducer.js
@@ -2,8 +2,18 @@ import { initialState } from "../store/index.js";
 
 const playingSongReducer = (state = initialState.playingSong, action) => {
   switch (action.type) {
+    case 'SONG_IS_PLAYING':
+      return {
+        ...state,
+        song: [...state.song, action.payload],
+      }
+    case 'SONG_IS_NOT_PLAYING':
+      return {
+        ...state,
+        song: [...state.song.slice(0, action.payload), ...state.song.slice(action.payload + 1)],
+      }
     default:
-      return state;
+      return state
   }
 };
 
